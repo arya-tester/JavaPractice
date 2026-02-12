5a4960610b8075279eaa79d2c0424294006370b4
Arya <arya28051996@gmail.com>
2026-02-12 07:24:16 +0530

    First Commit

---
+++ src/javapack/TestThis.java
@@ -0,0 +1,17 @@
+package javapack;
+class student{
+	int rollno;
+	String name;
+	Student(){
+		
+	}
+}
+
+public class TestThis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
